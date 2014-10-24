e086bedff814e48bfc6c6b2cc68affb74a40f626
mouse <mouse@sociomantic.com>
2014-10-24 11:02:31 +0200

    more cleaning up the code

--- _val.js
+++ _val.js
@@ -5,6 +5,7 @@ var channel, _bot, doge,
     userConfig      = require( './_val.config.js' ),
     channels        = userConfig.channels,
     Doge            = require( './src/doge.js' ),
+    PlainText       = require( './src/plainText.js' ),
     http            = require( 'http' ),
     https           = require( 'https' ),
     irc             = require( 'irc' ),
@@ -12,7 +13,6 @@ var channel, _bot, doge,
     nouns           = require( './lists/nouns.js' ),
     coffees         = require( './lists/coffee.js' ),
     cars            = require( './lists/cars.js' ),
-    moonRegex       = /(?:m([o]+)n)/,
     active          = {},
     dcMasterList    = {};
 
@@ -69,146 +69,6 @@ function apiGet( _url, _cb, secure )
             console.log( 'Got error: ', e );
         });
     }
-}
-
-
-function plainTextResponses( from, to, text, botText )
-{
-    var command = text.slice( 1 ).split( ' ' )[ 0 ];
-    var moon = moonRegex.exec( command );
-
-    if ( command.slice( command.length - 3 ) === 'end' )
-    {
-        var num = Math.floor( Math.random() * ( nouns.length ) );
-        var noun = nouns[ num ];
-
-        botText = command + 's ' + noun[ 0 ];
-
-        var target = text.split( ' ' );
-
-        if ( target && target[ 1 ] )
-        {
-            var connections = [ ' to ', ' at ' ];
-            num = Math.floor( Math.random() * ( connections.length ) );
-            botText += connections[ num ] + target[ 1 ];
-        }
-        _bot.action( from, botText );
-        botText = '';
-
-    }
-    else if ( command.slice( command.length - 5 ) === 'fetti' )
-    {
-        var type = command.slice( 0, command.length - 5 );
-        var word = type;
-
-        switch ( type )
-        {
-            case 'doge':
-                word = 'wow';
-                break;
-            case 'con':
-                word = '´ . \'';
-                break;
-        }
-
-        if ( type.length > userConfig.fettiLength )
-        {
-            word = 'toolong';
-        }
-
-        var option, options = userConfig.fettiOptions;
-        options[ 0 ] = word + ' ';
-        for (var  i = 0; i < 25; i++ )
-        {
-            option = Math.floor( Math.random() * options.length );
-            botText += options[ option ];
-        }
-    }
-    else if ( moon && moon[1] )
-    {
-        botText = 'm';
-        var moonLength = moon[1].length;
-        for ( var j = 0; j < moonLength; j++ )
-        {
-            botText += 'ooo';
-        }
-        botText += 'n';
-
-        if ( moonLength < 4 )
-        {
-            botText = 'To the ' + botText + '!';
-        }
-        if ( moonLength > 6 )
-        {
-            botText = botText.toUpperCase() + '!!!!!!!!';
-        }
-    }
-    else
-    {
-        switch ( command )
-        {
-            case 'rain':
-                botText = 'ヽ｀、ヽ｀ヽヽ｀、ヽ｀ヽヽ｀、ヽ｀ヽ(¬_¬ )ヽ｀、ヽ｀ヽ｀、ヽ｀';
-                break;
-            case 'dance':
-                botText = '♪┏(・o･)┛♪┗ ( ･o･) ┓♪';
-                break;
-            case 'domo':
-                botText = '\\|°▿▿▿▿°|/';
-                break;
-            case 'barrelroll':
-                botText = '(._.)  ( l: )  ( .-. )  ( :l )  (._.)';
-                break;
-            case 'hedgehog':
-                botText = '(•ᴥ• )́`́\'́`́\'́⻍ ';
-                break;
-            case 'wave':
-                botText = to + ' o/';
-                break;
-            case 'internet':
-                botText = 'ଘ(੭*ˊᵕˋ)੭* ̀ˋ ɪɴᴛᴇʀɴᴇᴛs';
-                break;
-            case 'cornflakes':
-            case 'snowflakes':
-                botText = '❅ ❆ ❄ ❆ ❆ ❄ ❆ ❅ ❆ ❆ ❅ ❆ ❄ ❄ ❅ ❄ ❆ ❆ ❆ ❄ ❆ ❆ ❄ ❆ ❆ ❅ ❅ ❄ ❄ ❅ ❄ ❄ ❄ ❆ ❄ ❅ ❆ ❅ ❅ ❄';
-                break;
-            case 'whale':
-                botText = 'https://www.youtube.com/watch?v=xo2bVbDtiX8';
-                break;
-            case 'bot':
-                botText = 'I AM BOT\nINSERT DOGE';
-                break;
-            case 'google':
-                text = text.split( ' ' ).slice( 1 ).join( '%20' );
-                botText = 'http://www.lmgtfy.com/?q=' + text;
-                break;
-            case 'g':
-                text = text.split( ' ' ).slice( 1 ).join( '%20' );
-                botText = 'https://www.google.com/search?btnG=1&pws=0&q=' + text + '&gws_rd=ssl';
-                break;
-            case 'witchhunt':
-                botText = 'http://i.imgur.com/x63cdJW.jpg';
-                break;
-            case 'innovation':
-                botText = 'INNOVATION!';
-                break;
-            case 'flipthetable':
-                botText = '(╯°□°）╯︵ ┻━┻';
-                break;
-            case 'chilloutbro':
-            case 'putthetableback':
-                botText = '┬──┬ ノ( ゜-゜ノ)';
-                break;
-            case 'vampire':
-                botText = '(°,..,°)';
-                break;
-            case 'ping':
-                botText = to + ': pong';
-                break;
-        }
-    }
-
-    return botText;
 }
 
 
@@ -318,6 +178,8 @@ function init()
 
     doge = new Doge( _bot, apiGet, userData );
 
+    plainText = new PlainText( _bot, apiGet, userData );
+
     doge.loadMasterList();
 }
 
@@ -355,7 +217,8 @@ function listenToMessages( from, to, text )
         }
         else if ( text[ 0 ] === userConfig.trigger && text !== userConfig.trigger )
         {
-            botText = plainTextResponses( from, to, text, botText );
+            botText = plainText( from, to, text, botText );
+
             if ( botText === '' )
             {
                 botText = dogeResponses( from, to, text, botText );
