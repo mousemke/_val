f12d255ac558adaa9d233cfeb3c13791cbd40d17
mouse <mouse@sociomantic.com>
2015-01-08 19:49:41 +0100

    4sq tweaks

--- src/_4sq.js
+++ src/_4sq.js
@@ -10,21 +10,22 @@ module.exports  = function _4sq( _bot, apiGet, userData, userConfig, doge )
         lunch : function( from, query )
         {
             var _botText;
+
             //DEFAULT i suppose
             var section = 'food';
-            var radius = '1000';
+            var radius  = '1000';
 
             var noSpaces = new RegExp( ' ', 'g' );
 
             query = query.split( ' ' ).slice( 1 ).join( '%20' );
 
-            var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + userConfig.foursquareID + '&client_secret=' + userConfig.foursquareSecret + '&v=20130815%20&ll=' + userConfig.latLong + '&llAcc=1000&openNow=1&limit=50&radius=' + radius + '&section=' + section;
+            var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + userConfig.foursquareID + '&client_secret=' + userConfig.foursquareSecret + '&v=20130815%20&ll=' + userConfig.latLong + '&llAcc=1000&openNow=1&radius=' + radius + '&section=' + section;
 
             if ( query !== '' )
             {
                  url += '&query=' + query;
             }
-console.log( query + ' ' + url );
+
             apiGet( url, function( result )
             {
                 var _valsChoice;
@@ -48,7 +49,12 @@ console.log( query + ' ' + url );
                     var tip     = _valsChoice.tips;
                     var tips    = tip.length;
                     tip         = tip[ Math.floor( Math.random() * tips ) ];
-                    var tipUser = tip.user.firstName + ' ' + tip.user.lastName;
+                    var tipUser = tip.user.firstName;
+                    if ( tip.user.lastName )
+                    {
+                        tipUser += ' ' + tip.user.lastName;
+                    } 
+
                     tip         = tipUser + ' says, "' + tip.text + '"';
 
                     _botText = 'Try ' + venue + '\n' + address;
@@ -65,6 +71,11 @@ console.log( query + ' ' + url );
                     {
                          _botText += '\n' + tip;
                     }
+
+                    var venueUrl = _valsChoice.venue.name.replace( noSpaces, '-' ) + 
+                                '/' + _valsChoice.venue.id;
+
+                    _botText += '\nhttps://foursquare.com/v/' + venueUrl;
                 }
 
                 _bot.say( from, _botText );
@@ -85,6 +96,7 @@ console.log( query + ' ' + url );
             switch ( command )
             {
                 case 'lunch':
+                case 'feedme':
                     this.lunch( from, text );
                     break;
             }
